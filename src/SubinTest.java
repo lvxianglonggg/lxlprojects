7c6a53162c2b8fa0e0f666249bc1fec409babefb
lvxianglong <lvxianglonggg@gmail.com>
2019-01-02 20:09:08 +0800

    该另一个程序员写了

---
+++ src/SubinTest.java
@@ -0,0 +1,11 @@
+/**
+ * @description: subin code
+ * @author: lxl
+ * @create: 2019-01-02 20:05
+ **/
+public class SubinTest {
+
+    public static void main(String[] args) {
+        System.out.println("快来写代码，素彬");
+    }
+}
