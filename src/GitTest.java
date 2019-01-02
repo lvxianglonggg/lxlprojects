5e0ee990dcfced08e86f38416c852b2532c476d9
lvxianglong <lvxianglonggg@gmail.com>
2019-01-02 19:52:01 +0800

    这才提交成功，刚才失败

---
+++ src/GitTest.java
@@ -0,0 +1,12 @@
+/**
+ * @program: lxlprojects
+ * @description: git项目的测试
+ * @author: lxl
+ * @create: 2019-01-02 19:36
+ **/
+public class GitTest {
+
+    public static void main(String[] args) {
+        System.out.println("git hello world");
+    }
+}
